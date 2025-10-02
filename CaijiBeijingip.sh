3e9da6879bd235971911f4d5074229a0345b73ba
ykyfy1 <yktcfy@163.com>
2025-10-02 10:47:46 +0800

    Update CaijiBeijingip.sh

--- CaijiBeijingip.sh
+++ CaijiBeijingip.sh
@@ -18,7 +18,7 @@ fi
 case $city_choice in
     1)
         city="Beijing_dianxin"
-        stream="rtp/239.45.3.237:5140"
+        stream="rtp/239.16.20.195:11950"
         ;;
     0)
         # 逐个处理{ }内每个选项
